0e111c8783d499880a7b108a9a99e30f1a8fe359
dalesusan1 <58398972+dalesusan1@users.noreply.github.com>
2020-06-10 16:52:35 -0500

    secod

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -4,6 +4,8 @@ var tableData = data;
 // YOUR CODE HERE!
 var output = d3.select("tbody");
 
+
+// generating the entire table
 tableData.forEach((ufo) => {
     console.log(ufo);
     var row = output.append("tr");
@@ -17,53 +19,33 @@ tableData.forEach((ufo) => {
 // select button
 var button = d3.select("button#filter-btn");
 
-// select input
-var inputField = d3.select("input#datetime")
-
-
-function filter (
-  
-)
-
 // change function
+button.on("click", handleChange);
 
+// creating the function handleChange
 function handleChange() {
-  var inputText = d3.event.target.value;
+  // prevent refreshing the page
+  d3.event.preventDefault();
+
+  // select input field
+  var inputField = d3.select("input#datetime");
+  var inputValue = inputField.property("value");
+  console.log(inputValue)
+
+  // filter data
+  var filteredData = tableData.filter(ufoData => ufoData.datetime === inputValue);
+  console.log(filteredData);
+  // clearing output
   output.text("");
-  var outputText = filter(inputText)
-  output.text(outputText);
-
+  // generating table with the filtered data
+  filteredData.forEach((ufo) => {
+      console.log(ufo);
+      var row = output.append("tr");
+          Object.entries(ufo).forEach(function([key, value]) {
+            console.log(key, value);
+            var cell = row.append("td");
+            cell.text(value);
+      });
+    });
 }
-
-button.on("click", handleChange)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
-// inputField.on("change", function() {
-//     var newText = d3.event.target.value
-//     console.log(newText)
-//     tbody.text(newText)
-//     }
-// );
-
-// // click function
-// button.on("click", function() {
-//       console.log("button clicked");
-//       // filtered data function;
-//   }
-// );
 
